650a873a66ec1937c95169cc850fbfe1642c5853
JaraspongSabpaiboon <biacktokung@gmail.com>
2024-01-30 14:17:15 +0700

    Labsheet11

---
+++ Labsheet11/src/Book.java
@@ -0,0 +1,8 @@
+public interface Book {
+
+	//public abstract method
+	public abstract String getTitle();
+
+	public abstract int totalPublicYear();
+
+}
---
+++ Labsheet11/src/FictionDemo1.java
@@ -0,0 +1,4 @@
+
+public class FictionDemo1 {
+
+}
