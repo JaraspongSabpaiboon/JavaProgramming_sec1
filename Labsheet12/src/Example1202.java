1cf3bda46b35917ca29500181d11f28062b5af59
JaraspongSabpaiboon <biacktokung@gmail.com>
2024-02-06 14:23:08 +0700

    Labsheet12

--- Labsheet12/src/Example1202.java
+++ Labsheet12/src/Example1202.java
@@ -1,42 +1,46 @@
-import java.util.Scanner;
+import java.util.*;
 import java.io.*;
 public class Example1202 {
+
 	public static void main(String[] args) throws IOException {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Input Section:");
+		System.out.print("Input Section : ");
 		int section = input.nextInt();
-		Header(section);
+		header(section);
 		showListStudent(section);
- 
+
 	}
-	public static void showListStudent(int section) throws IOException{
-		//read data from file
-		BufferedReader readFile = new BufferedReader (new FileReader("d://txtFile//List107.txt"));
-		String tmp = "";
-		while((tmp= readFile.readLine())!= null) {
-			//read midtem and final
-			double midtemScore = Double.parseDouble(data[4]);
-			double finalScore = Double.parseDouble(data[5]);
+	public static void showListStudent(int section ) throws IOException{
+		BufferedReader read = new BufferedReader(new FileReader("D:\\txtFile\\List107.txt"));
+		String temp = "";
+
+		while ((temp = read.readLine()) !=null) {
+			String[] data = temp.split("\t");
+			double mid = Double.parseDouble(data[4]);
+			double Final = Double.parseDouble(data[5]);
 			int sectionFile = Integer.parseInt(data[3]);
-			
-			if(sectionFile==sectionInput){
-				System.out.println(data[0]+"\t"+data[2]+"\t"+midtemScore+"\t"+finalScore+"\t"+findResult(midtemScore,finalScore));
+			if(sectionFile==section) {
+			System.out.println(data[0]+"\t"+data[2]+"\t"+mid+"\t"+Final+"\t"+findResult(mid,Final));
+
 			}
 		}
-		readFile.close();
-		
+
+
 	}
-	public static String findResult(double midTermScore,double finalScore) {
-		double totalScore = midtemScore+finalscore;
-		/* id(totalScore<=40) return "Fail";
-		   else return "Pass";*/
-		 
-		return "";
+	public static String findResult(Double Mid, Double Final)
+	{
+		if(Mid+Final>=40) {
+			return "PASS";
+		}
+		else {
+			return "FALL";
+		}
+
 	}
-	public static void Header(int sec) {
-		System.out.print("**************************************************");
-		System.out.print("\tLIst of Data for Section"+ sec);
-		System.out.print("**************************************************");
+
+	public static void header(int section) {
+		System.out.println("*****************************************************************");
+		System.out.println("\t\tList of Data for Section "+(section == 1 ? "1":"2"));
+		System.out.println("*****************************************************************");
 	}
- 
-}//end classes
+}
